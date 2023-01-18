271ccec339df5a877f7abd606851f6667a4b077e
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2023-01-18 02:34:02 -0300

    Iniciando projeto

---
+++ src/main/java/com/wandeilson/acermoneyapi/AcerMoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.wandeilson.acermoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcerMoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcerMoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wandeilson/acermoneyapi/AcerMoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wandeilson.acermoneyapi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcerMoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
