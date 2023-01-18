b44dcf3af82671326d94c02f0f2ea9ccb365866b
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2023-01-18 04:52:25 -0300

    Altera para fins de aprendizado e desfaz. Não mudou nada.

--- src/main/java/com/wandeilson/acermoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/wandeilson/acermoneyapi/resource/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.wandeilson.acermoneyapi.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,4 @@ public class CategoriaResource {
 	public List<Categoria> listar (){
 		return categoriaRepository.findAll();
 	}
-	
 }
