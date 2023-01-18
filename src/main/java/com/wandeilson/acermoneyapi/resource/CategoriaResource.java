811b4410f82106b2e5e990c5f0b9eefc734a8256
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2023-01-18 05:22:55 -0300

    Cria metodo POST para salvar categoria e GET para recuperar por codigo

--- src/main/java/com/wandeilson/acermoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/wandeilson/acermoneyapi/resource/CategoriaResource.java
@@ -1,12 +1,21 @@
 package com.wandeilson.acermoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wandeilson.acermoneyapi.model.Categoria;
 import com.wandeilson.acermoneyapi.repository.CategoriaRepository;
@@ -22,4 +31,25 @@ public class CategoriaResource {
 	public List<Categoria> listar (){
 		return categoriaRepository.findAll();
 	}
+	@PostMapping
+	@ResponseStatus (HttpStatus.CREATED)
+	public ResponseEntity<Categoria> salvar (@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria catSalva = categoriaRepository.save(categoria);
+		//Utilizado p informar onde (url) foi salvo o novo registro. A info vai junto a response
+		//no 'location' do Header
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+		.buildAndExpand(catSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(catSalva);
+	}
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findOne(codigo);
+	}
+	
+	
+	
+	
+	
 }
