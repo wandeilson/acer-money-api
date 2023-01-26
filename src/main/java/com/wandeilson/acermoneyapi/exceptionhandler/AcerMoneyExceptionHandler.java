7d7d0c1dd1bd7143827d9239475cff4c4b19dbaa
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2023-01-26 19:34:35 -0300

    Tratamento de exceções ao criar novo lançamento

--- src/main/java/com/wandeilson/acermoneyapi/exceptionhandler/AcerMoneyExceptionHandler.java
+++ src/main/java/com/wandeilson/acermoneyapi/exceptionhandler/AcerMoneyExceptionHandler.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -42,6 +44,14 @@ public class AcerMoneyExceptionHandler extends ResponseEntityExceptionHandler{
 		List<Erro> erros = criarListaDeErros(ex.getBindingResult());
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
+	@ExceptionHandler({DataIntegrityViolationException.class})
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request){
+		String mensagemUsuario = messageSource.getMessage("operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario,  mensagemDesenvolvedor) );
+		return handleExceptionInternal(ex, erros ,new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+	}
+	
 	
 	@ExceptionHandler({EmptyResultDataAccessException.class})
 	public ResponseEntity<Object> handleEmptyResultDataAccessException( EmptyResultDataAccessException ex, WebRequest request ) {
