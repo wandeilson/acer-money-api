f62d4de74a61c93bc60379de6befdc6975a6f70f
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2023-03-20 16:03:10 -0300

    Adiciona paginacão de lançamentos

--- src/main/java/com/wandeilson/acermoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/wandeilson/acermoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,11 +1,12 @@
 package com.wandeilson.acermoneyapi.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.wandeilson.acermoneyapi.model.Lancamento;
 import com.wandeilson.acermoneyapi.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
--- src/main/java/com/wandeilson/acermoneyapi/resource/LancamentoResource.java
+++ src/main/java/com/wandeilson/acermoneyapi/resource/LancamentoResource.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -48,8 +50,8 @@ public class LancamentoResource {
 	
 	@GetMapping()
 	@ResponseStatus(HttpStatus.OK)
-	public List<Lancamento> pesquisar(LancamentoFilter lancametoFilter){
-		return lancamentoRepository.filtrar(lancametoFilter);
+	public Page<Lancamento> pesquisar(LancamentoFilter lancametoFilter, Pageable pageable){
+		return lancamentoRepository.filtrar(lancametoFilter, pageable);
 	}
 	
 	@GetMapping("/{codigo}")
